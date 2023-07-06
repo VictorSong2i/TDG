6f80bac8d612ac847c61e1b4d8e8e23f4d68500f
VictorSong <victor.song@2itesting.com>
2023-07-06 15:59:51 +0100

    Added comments

--- cypress/e2e/TDG/GenerateTestData.cy.js
+++ cypress/e2e/TDG/GenerateTestData.cy.js
@@ -27,6 +27,7 @@ describe('generate test data', () => {
       cy.contains(title[i]).click()
     }
 
+    //Medical fields
     cy.get('input#medical_input').click()
     for (let i = 0; i < gpDetails.length; i++) {
       cy.contains(gpDetails[i]).click()
@@ -63,41 +64,33 @@ describe('generate test data', () => {
   })
 
   it('Check contents of csv and json file match',()=>{
-    cy.task('unzipping', { path, file}) //Unzip file
+    cy.task('unzipping', { path, file}) //Unzip file into its csv and 
     let renameFile = file.substring(0,file.length-4) //Deletes .zip in name
 
     //Assign paths to each file type
     let csvFilePath = path.concat('unzip/',renameFile,'/CSV1.csv')
     let jsonFilePath = path.concat('unzip/',renameFile,'/JSON1.json')
+
+    //Reads csv downlaoded
     cy.readFile(csvFilePath).then((csvFile)=>{
-      let lines = csvFile.split('\n')
-      let fields = lines[0].split(',')
-      cy.log(lines[0])
-      cy.log(lines[0][0])
-      let varNames = lines[0].split(',')
-      cy.log(varNames)
-      expect(lines.length).to.equal(numData+1)
-      expect(fields.length).to.equal(mergedDetails.length)
+
+      //CSV lines
+      let lines = csvFile.split('\n') //Splits csv into lines
+      let fields = lines[0].split(',') //Creates data field array
+
+      expect(lines.length).to.equal(numData+1) //Checks number of entries, +1 because of extra line for field names
+      expect(fields.length).to.equal(mergedDetails.length) //Check number of fields
       
+      //Compare field name in downloaded file to 
       cy.fixture("dataName.json").then((properties)=>{
-          cy.log(properties)
-          cy.log(varNames[0].substring(1,varNames[0].length-1))
-          cy.log(varNames[1])
-          
-          // // cy.log(Object.keys(properties).find(key => properties[key] === varNames[mergedDetails[1]]))
-          // cy.log(Object.keys(properties).find(key => properties[key] == varNames[i].substring(1,varNames[i].length-1)))
+          cy.log(properties) //Properties - dictionary for matching field names on website to csv/json field names
       
+          //Checks if fields in downloaded file exist in dictionary dataName using expect
           for(let i=0;i < mergedDetails.length; i++){
-            cy.log(Object.keys(properties).find(key => properties[key] == varNames[i].substring(1,varNames[i].length-1)))
-           expect(Object.keys(properties).find(key => properties[key] == varNames[i].substring(1,varNames[i].length-1))).to.exist
+          // cy.log(Object.keys(properties).find(key => properties[key] == fields[i].substring(1,fields[i].length-1)))
+          expect(Object.keys(properties).find(key => properties[key] == fields[i].substring(1,fields[i].length-1))).to.exist 
           }
-          
-          
       })
     })
-
-    // cy.readFile(jsonFilePath).then((jsonFile)=>{
-    //   cy.log(Object.keys(jsonFile))
-    // })
   })
 })
