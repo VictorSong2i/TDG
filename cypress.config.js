9ddb09c1bc3262816ce3e27d789c9b9070b2c96c
VictorSong <victor.song@2itesting.com>
2023-07-07 12:35:44 +0100

    Added TDG all tests and fixtures

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
+
+
 module.exports = {
+
+  defaultCommandTimeout: 10000,
+  projectId: "d75n5r",
+  defaultCommandTimeout: 10000,
+  reporter: "junit",
+
+  reporterOptions: {
+    mochaFile: "cypress/results/junit-results-[hash].xml",
+    toConsole: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      
       return require('./cypress/plugins/index.js')(on, config)
-
     },
   },
 };
